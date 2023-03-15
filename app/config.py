24d0e893f6998a1bf0971c6ccd7724292c05c1bd
Jonathan Owens <115698235+jonathan-owens-gds@users.noreply.github.com>
2023-03-15 10:03:28 +0000

    EAS-447 Containerized build image (#20)
    * reduce layer count in dockerfile
    
    * reduce session timeout from 20 hrs to 30 mins
    
    * Correct text describing session timeout duration
    
    * Comments updated to reflect actual timeout operation

--- app/config.py
+++ app/config.py
@@ -42,7 +42,7 @@ class Config(object):
     HTTP_PROTOCOL = "http"
     NOTIFY_APP_NAME = "admin"
     NOTIFY_LOG_LEVEL = "DEBUG"
-    PERMANENT_SESSION_LIFETIME = 20 * 60 * 60  # 20 hours
+    PERMANENT_SESSION_LIFETIME = 30 * 60  # 30 minutes
     SEND_FILE_MAX_AGE_DEFAULT = 365 * 24 * 60 * 60  # 1 year
     SESSION_COOKIE_HTTPONLY = True
     SESSION_COOKIE_NAME = "notify_admin_session"
