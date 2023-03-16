914a4bb61dd55b029a4c6b3704d3516eceb578e1
Adam Mitchell <adam.mitchell@digital.cabinet-office.gov.uk>
2023-03-16 06:54:54 +0000

    Add frontend write new broadcast happy path tests

--- tests/app/main/views/test_broadcast.py
+++ tests/app/main/views/test_broadcast.py
@@ -11,6 +11,7 @@ from tests import broadcast_message_json, sample_uuid, user_json
 from tests.app.broadcast_areas.custom_polygons import BRISTOL, SKYE
 from tests.conftest import (
     SERVICE_ONE_ID,
+    SERVICE_TWO_ID,
     create_active_user_approve_broadcasts_permissions,
     create_active_user_create_broadcasts_permissions,
     create_active_user_view_permissions,
@@ -1465,6 +1466,66 @@ def test_preview_broadcast_areas_has_back_link_with_uuid(
         broadcast_message_id=str(uuid.UUID(int=0)),
         **expected_back_link_extra_kwargs,
     )
+
+
+def test_write_new_broadcast_content_from_uuid_is_displayed_before_live(
+    mocker,
+    client_request,
+    service_one,
+    fake_uuid,
+    active_user_create_broadcasts_permission,
+):
+    mocker.patch(
+        "app.broadcast_message_api_client.get_broadcast_message",
+        return_value=broadcast_message_json(
+            id_=fake_uuid,
+            reference="Emergency broadcast",
+            content="Emergency broadcast content",
+            template_id=fake_uuid,
+            created_by_id=fake_uuid,
+            service_id=SERVICE_ONE_ID,
+            status="draft",
+        )
+    )
+    service_one["permissions"] += ["broadcast"]
+    client_request.login(active_user_create_broadcasts_permission)
+    page = client_request.get(
+        "main.write_new_broadcast", service_id=SERVICE_ONE_ID, broadcast_message_id=fake_uuid
+    )
+
+    assert normalize_spaces(page.select_one("textarea").text) == "Emergency broadcast content"
+
+
+def test_write_new_broadcast_only_displays_from_this_service(
+    mocker,
+    client_request,
+    service_one,
+    service_two,
+    active_user_create_broadcasts_permission,
+):
+    service_one["permissions"] += ["broadcast"]
+    client_request.login(active_user_create_broadcasts_permission)
+    page = client_request.get(
+        "main.write_new_broadcast", service_id=SERVICE_TWO_ID, broadcast_message_id=str(uuid.UUID(int=0)),
+        _expected_status=403,
+    )
+    assert normalize_spaces(page.select_one("h1").text) == "You’re not allowed to see this page"
+
+
+def test_write_new_broadcast_does_not_display_alerts_in_broadcast(
+    mocker,
+    client_request,
+    service_one,
+    mock_get_live_broadcast_message,
+    fake_uuid,
+    active_user_create_broadcasts_permission,
+):
+    service_one["permissions"] += ["broadcast"]
+    client_request.login(active_user_create_broadcasts_permission)
+    page = client_request.get(
+        "main.write_new_broadcast", service_id=SERVICE_ONE_ID, broadcast_message_id=fake_uuid
+    )
+    assert normalize_spaces(page.select_one("input").text) == ""
 
 
 def test_choose_broadcast_sub_area_page_for_county_shows_links_for_districts(
