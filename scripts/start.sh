fd99b1de12eccbd03be25ee3c17454222e4a1341
Lewis Stevens <lewis.stevens@digital.cabinet-office.gov.uk>
2023-05-15 22:06:24 +0100

    Adding a sleep to the debug call

--- scripts/start.sh
+++ scripts/start.sh
@@ -13,6 +13,7 @@ function run_admin(){
 
 if [[ ! -z $DEBUG ]]; then
     echo "Starting in debug mode.."
+    while true; do echo 'Debug mode active..'; sleep 30; done
 else
     configure_container_role
     run_admin
