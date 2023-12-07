87d6c6be80f5d2395b3e3da7b4bcba5b75932e63
Lewis Stevens <lewis.stevens@digital.cabinet-office.gov.uk>
2023-12-07 11:52:50 +0000

    Change debug flag to string validation

--- scripts/healthcheck-admin.sh
+++ scripts/healthcheck-admin.sh
@@ -4,7 +4,7 @@ function check_status_endpoint(){
   curl -f 0.0.0.0:6012/_admin_status || exit 1
 }
 
-if [[ ! -z $DEBUG ]]; then
+if [[ $DEBUG == "true" ]]; then
   echo "Debug mode active.."
 else
   check_status_endpoint
