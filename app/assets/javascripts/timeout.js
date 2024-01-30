f1b083ceb0c700cc04c1ad5c8b516ebb39925d77
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2024-01-30 10:49:27 +0000

    CSS change and added comments

--- app/assets/javascripts/timeout.js
+++ app/assets/javascripts/timeout.js
@@ -2,11 +2,12 @@ let initial_popup;
 let initial_logout;
 
 const inactivity_mins = 0.5; // to display first popup
-const warning_mins = 1; // to logout if stay signed in not chosen
+const initial_warning_mins = 1; // to logout if no response after popup displayed
 const timeout_warning_mins = 4; // // to display final popup before logged out by flask
 
 let loggedInTimestamp = new Date(logged_in_at);
-let isLoggedIn = (loggedInTimestamp instanceof Date) & !isNaN(loggedInTimestamp);
+let isLoggedIn =
+  (loggedInTimestamp instanceof Date) & !isNaN(loggedInTimestamp);
 
 let inactivity_popup = document.getElementById("activity");
 let session_expiry_popup = document.getElementById("expiry");
@@ -18,9 +19,27 @@ let signInAgainButton = document.getElementById(
   "hmrc-timeout-sign-in-again-btn"
 );
 
-function startInactivityTimeout() {
+function getTimeLeftUntilExpiry() {
+  // calculates how long left in session before final timeout after each request
+  let expiry_time = new Date();
+  expiry_time.setMinutes(loggedInTimestamp.getMinutes() + timeout_warning_mins);
+  let current_time = new Date();
+  let timeLeft = expiry_time - current_time;
+  return timeLeft;
+}
+
+function displayInactivityPopup() {
+  // check if user logged in and there is enough time left before session expires
   timeLeft = getTimeLeftUntilExpiry();
-  if (isLoggedIn & (timeLeft > (timeout_warning_mins - inactivity_mins) * 60 * 1000)) {
+  return (
+    isLoggedIn &
+    (timeLeft > (timeout_warning_mins - inactivity_mins) * 60 * 1000)
+  );
+}
+
+function startInactivityTimeout() {
+  // initial timeout that is restarted by request activity
+  if (displayInactivityPopup()) {
     initial_popup = setTimeout(function () {
       if (checkLocalStorage()) {
         lastActiveTimeout();
@@ -32,13 +51,8 @@ function startInactivityTimeout() {
   }
 }
 
-function startInactivityPopupTimeout() {
-  initial_logout = setTimeout(function () {
-    window.location.href = "/sign-out";
-  }, 1000 * 60 * warning_mins);
-}
-
 function resetTimeouts() {
+  // if stay signed in button is clicked then activity timeout is restarted
   if (staySignedInButton) {
     staySignedInButton.addEventListener("click", function () {
       clearTimeout(initial_popup);
@@ -49,7 +63,15 @@ function resetTimeouts() {
   }
 }
 
+function startInactivityPopupTimeout() {
+  // logs user out if no response when the inactivity popup shows
+  initial_logout = setTimeout(function () {
+    window.location.href = "/sign-out";
+  }, 1000 * 60 * initial_warning_mins);
+}
+
 function sessionExpiryPopup() {
+  // displays session expiry popup after timeout
   if (isLoggedIn) {
     initial_logout = setTimeout(function () {
       session_expiry_popup.open = true;
@@ -57,15 +79,8 @@ function sessionExpiryPopup() {
   }
 }
 
-function getTimeLeftUntilExpiry() {
-  let expiry_time = new Date();
-  expiry_time.setMinutes(loggedInTimestamp.getMinutes() + timeout_warning_mins);
-  let current_time = new Date();
-  let timeLeft = expiry_time - current_time;
-  return timeLeft;
-}
-
 function signInAgain() {
+  // if signin button in final timeout popup is clicked, user is signed out and upon logging back in is redirected to previous page
   if (signInAgainButton) {
     signInAgainButton.addEventListener("click", function () {
       signOutRedirect();
@@ -75,41 +90,40 @@ function signInAgain() {
 }
 
 function updateLocalStorage() {
+  // with each request in any tab, the lastActive attribute is updated
   localStorage.setItem("lastActivity", new Date());
 }
 
 function checkLocalStorage() {
-  let lastActive = new Date (localStorage.getItem("lastActivity"));
+  // checking local storage for any activity in other tabs
+  let lastActive = new Date(localStorage.getItem("lastActivity"));
   let time = new Date();
-  return ((time - lastActive) < (inactivity_mins * 60 * 1000));
+  return time - lastActive < inactivity_mins * 60 * 1000;
 }
 
 function lastActiveTimeout() {
+  // if activity in another tab, inactivity timeout period adjusted
   let time = new Date();
   let lastActive = new Date(localStorage.getItem("lastActivity"));
   setTimeout(() => {
     inactivity_popup.open = true;
     startInactivityPopupTimeout();
-  }, (mins * 60 * 1000) - (time - lastActive));
+  }, inactivity_mins * 60 * 1000 - (time - lastActive));
 }
 
 function signOutRedirect() {
+  // send logout request and redirect to previous page upon relogging in
   let current_page = window.location.pathname;
-  $.ajax(
-    '/sign-out',
-    {
-      'method': 'get',
-    }
-  ).done(
-    response => {
+  $.ajax("/sign-out", {
+    method: "get",
+  })
+    .done((response) => {
       window.location.href = current_page;
       console.log(response);
-    }
-  ).fail(
-    response => {
+    })
+    .fail((response) => {
       console.log(response);
-    }
-  );
+    });
 }
 
 updateLocalStorage();
