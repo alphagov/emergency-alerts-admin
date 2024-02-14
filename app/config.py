9828cecdb33a998ea1411d35ca0e042c65c51eb6
Adam Mitchell <118552485+Adam-C-Mitchell@users.noreply.github.com>
2024-02-14 11:07:33 +0000

    EAS 1736 Multi Tenant Dev - tenant prefixing (#108)
    * Enable per-tenant ECR base image
    
    * Defaulting RESOURCE_PREFIX for compatbility
    
    * Use resource prefix variable in buildspec
    
    * Multitenanted webauthn rpid
    
    * Tenant prefixing service discovery urls
    
    * Fixing Docker CLI syntax mistake
    
    * Webauthn subdomain fix for dev/development
    
    * Clumsy ternaries

--- app/config.py
+++ app/config.py
@@ -116,14 +116,21 @@ class Config(object):
 
 class Hosted(Config):
     HOST = "hosted"
-    API_HOST_NAME = "http://api.ecs.local:6011"
-    ADMIN_BASE_URL = "http://admin.ecs.local:6012"
+    TENANT = f"{os.environ.get('TENANT')}." if os.environ.get("TENANT") is not None else ""
+    SUBDOMAIN = (
+        "dev."
+        if os.environ.get("ENVIRONMENT") == "development"
+        else f"{os.environ.get('ENVIRONMENT')}."
+        if os.environ.get("ENVIRONMENT") != "production"
+        else ""
+    )
+    API_HOST_NAME = f"http://api.{TENANT}ecs.local:6011"
+    ADMIN_BASE_URL = f"http://admin.{TENANT}ecs.local:6012"
     HEADER_COLOUR = header_colors.get(os.environ.get("ENVIRONMENT"), "#81878b")
-    SUBDOMAIN = f"{os.environ.get('ENVIRONMENT')}." if os.environ.get("ENVIRONMENT") != "production" else ""
-    ADMIN_EXTERNAL_URL = f"https://admin.{SUBDOMAIN}emergency-alerts.service.gov.uk"
-    TEMPLATE_PREVIEW_API_HOST = "http://api.ecs.local:6013"
-    ANTIVIRUS_API_HOST = "http://admin.ecs.local:6016"
-    REDIS_URL = "redis://api.ecs.local:6379/0"
+    ADMIN_EXTERNAL_URL = f"https://{TENANT}admin.{SUBDOMAIN}emergency-alerts.service.gov.uk"
+    TEMPLATE_PREVIEW_API_HOST = f"http://api.{TENANT}ecs.local:6013"
+    ANTIVIRUS_API_HOST = f"http://admin.{TENANT}ecs.local:6016"
+    REDIS_URL = f"redis://api.{TENANT}ecs.local:6379/0"
 
     DEBUG = False
     SESSION_COOKIE_SECURE = True
@@ -147,8 +154,15 @@ class Test(Config):
     ANTIVIRUS_API_HOST = "https://test-antivirus"
     ANTIVIRUS_API_KEY = "test-antivirus-secret"
     ANTIVIRUS_ENABLED = True
-    SUBDOMAIN = f"{os.environ.get('ENVIRONMENT')}." if os.environ.get("ENVIRONMENT") != "production" else ""
-    ADMIN_EXTERNAL_URL = f"https://admin.{SUBDOMAIN}emergency-alerts.service.gov.uk"
+    TENANT = f"{os.environ.get('TENANT')}." if os.environ.get("TENANT") is not None else ""
+    SUBDOMAIN = (
+        "dev."
+        if os.environ.get("ENVIRONMENT") == "development"
+        else f"{os.environ.get('ENVIRONMENT')}."
+        if os.environ.get("ENVIRONMENT") != "production"
+        else ""
+    )
+    ADMIN_EXTERNAL_URL = f"https://{TENANT}admin.{SUBDOMAIN}emergency-alerts.service.gov.uk"
     ASSET_DOMAIN = "static.example.com"
     ASSET_PATH = "https://static.example.com/"
 
