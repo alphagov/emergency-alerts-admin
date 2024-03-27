074fb75ce8dded089787521741e666c4084b9b6c
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2024-03-27 20:02:27 +0000

    Ensuring custom area string consistent across application

--- app/main/views/broadcast.py
+++ app/main/views/broadcast.py
@@ -373,7 +373,8 @@ def redirect_to_postcode_map(service_id, broadcast_message_id, form: PostcodeFor
     postcode = "postcodes-" + form.data["postcode"].upper()
     try:
         area = BroadcastMessage.libraries.get_areas([postcode])[0]
-        circle_id = f"an area of {form.data['radius']}km around {form.data['postcode'].upper()}"
+        radius_to_min_sig_figs = "{:g}".format(float(form.data["radius"]))
+        circle_id = f"an area of {radius_to_min_sig_figs}km around {form.data['postcode'].upper()}"
 
         centroid = get_centroid(area)
         circle_polygon = create_circle(centroid, float(form.data["radius"]) * 1000)
