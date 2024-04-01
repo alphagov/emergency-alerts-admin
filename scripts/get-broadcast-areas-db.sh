15744103a51a108aa3ef13844c8ddf665a99cb5e
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2024-04-01 18:52:04 +0100

    Ensuring quiet option enabled if QUIET_DOWNLOAD env var specified

--- scripts/get-broadcast-areas-db.sh
+++ scripts/get-broadcast-areas-db.sh
@@ -4,14 +4,14 @@ TARGET_LOCAL_DIR=$1
 SOURCE_BUCKET_DIR=$2
 QUIET_DOWNLOAD=$3
 
-if [ -n "$QUIET_DOWNLOAD" ]; then
-	quiet_flag="--quiet"
-fi
-
 if ! [ -f "$TARGET_LOCAL_DIR/broadcast-areas.sqlite3" ]; then
 	if aws s3 ls "s3://$SOURCE_BUCKET_DIR/broadcast-areas.sqlite3"; then
 		echo "Downloading Broadcast Areas Sqlite file..."
-		aws s3 cp "s3://$SOURCE_BUCKET_DIR/broadcast-areas.sqlite3" "$TARGET_LOCAL_DIR" "$quiet_flag"
+		if [ -n "$QUIET_DOWNLOAD" ]; then
+			aws s3 cp "s3://$SOURCE_BUCKET_DIR/broadcast-areas.sqlite3" "$TARGET_LOCAL_DIR" --quiet
+		else \
+			aws s3 cp "s3://$SOURCE_BUCKET_DIR/broadcast-areas.sqlite3" "$TARGET_LOCAL_DIR"
+		fi
 	else \
 		echo "Broadcast Areas Sqlite file required by application not found in S3 bucket. Exiting...";
 		exit 1;
