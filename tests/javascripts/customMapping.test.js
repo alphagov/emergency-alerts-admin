16f40de3d31e5c93eeb5fd0f306c3acdc2bd082b
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2024-05-20 07:52:04 +0100

    Added tests for customMapping.js

--- tests/javascripts/customMapping.test.js
+++ tests/javascripts/customMapping.test.js
@@ -5,7 +5,7 @@ const {createLatitudelongitudeArea, createEastingNorthingArea, createPostcodeAre
     createBleedCircle, createCoordinateAreaLabel, createPostcodeAreaLabel, appendAreaToAreaList
 } = require('../../app/assets/javascripts/customMapping.js')
 
-jest.createMockFromModule('leaflet');
+const L = require('leaflet');
 const proj4 = require('proj4');
 proj4.defs("EPSG:27700",
   "+proj=tmerc +lat_0=49 +lon_0=-2 +k=0.9996012717 +x_0=400000 +y_0=-100000 +ellps=airy +towgs84=446.448,-125.157,542.06,0.15,0.247,0.842,-20.489 +units=m +no_defs +type=crs");
@@ -17,9 +17,14 @@ let northing = 179963;
 let radius = 5000;
 let bleed = 7000;
 let postcode = 'SW1A 2AS';
-markerElement = document.querySelector('.leaflet-marker-icon')
+
+let mymap = {fitBounds: jest.fn(), setView: jest.fn(), addLayer: jest.fn()}
 
 beforeAll(() => {
+    require('./support/teardown.js');
+});
+
+beforeEach(() => {
     require('./support/teardown.js');
     markerIcon = L.icon({
         iconUrl:"test.jpg"
@@ -34,13 +39,12 @@ beforeAll(() => {
       </ul>
       <div id="area-list-map" role="region" aria-label="Map of the United Kingdom, showing no areas" aria-describedby="area-list-map__description" </div>`;
     mapElement = document.getElementById('area-list-map');
-    mymap = L.map(
-      'area-list-map',
-      {
-        scrollWheelZoom: false
-      }
-    );
 });
+
+afterEach(() => {
+    document.body.innerHTML = '';
+});
+
 afterAll(() => {
     require('./support/teardown.js');
 });
@@ -81,67 +85,93 @@ describe('Coordinate Area label is created when area is created using coordinate
 
     test("User has entered the latitude, longitude and radius and then searched to generate the area label", () => {
         createCoordinateAreaLabel('latitude_longitude', radius, latitude, longitude, mapElement);
-        expect(document.getElementById('area-list-map').getAttribute('aria-label')).toBe("Map of the United Kingdom, showing an area of " + radius / 1000 + "km around the latitude " + latitude.toFixed(2) + ", longitude " + longitude.toFixed(2));
+        expected_label = "Map of the United Kingdom, showing an area of " + radius / 1000 + "km around the latitude " + latitude.toFixed(2) + ", longitude " + longitude.toFixed(2);
+        expect(document.getElementById('area-list-map').getAttribute('aria-label')).toBe(expected_label);
     });
     test("User has entered the latitude, longitude and radius and then searched to generate the area label", () => {
         createCoordinateAreaLabel("easting_northing", radius, easting, northing, mapElement);
-        expect(document.getElementById('area-list-map').getAttribute('aria-label')).toBe("Map of the United Kingdom, showing an area of " + radius / 1000 + "km around the easting of " + easting.toFixed(0) + ", northing of " + northing.toFixed(0));
+        expected_label = "Map of the United Kingdom, showing an area of " + radius / 1000 + "km around the easting of " + easting.toFixed(0) + ", northing of " + northing.toFixed(0);
+        expect(document.getElementById('area-list-map').getAttribute('aria-label')).toBe(expected_label);
     });
 })
 
 describe('Postcode Area label is created when area is created using postcode and radius', () => {
 
     test("User has entered the postcode and radius and then searched to generate the leaflet circle and marker on the map", () => {
-
         createPostcodeAreaLabel(radius, mapElement, postcode);
-        expect(document.getElementById('area-list-map').getAttribute('aria-label')).toBe("Map of the United Kingdom, showing an area of " + radius / 1000 + "km around the postcode " + postcode);
+        expected_label = "Map of the United Kingdom, showing an area of " + radius / 1000 + "km around the postcode " + postcode;
+        expect(document.getElementById('area-list-map').getAttribute('aria-label')).toBe(expected_label);
     });
 
 });
 
 describe('Latitude and longitude area is created on the map when latitude and longitude searched and radius entered', () => {
-
+    let bleed_circle = {addTo: jest.fn(), getBounds: jest.fn().mockReturnValue({extend: jest.fn()})};
+    let circle = {addTo: jest.fn(), getBounds: jest.fn().mockReturnValue({extend: jest.fn()})};
+    let pinpoint = {addTo: jest.fn(), getLatLng: jest.fn().mockReturnValue([0,0])};
     test("User has entered the postcode and radius and then searched to generate the leaflet circle and marker on the map", () => {
-
         createLatitudelongitudeArea(L, latitude, longitude, radius, bleed, mapElement);
-        expect(document.getElementById('area-list-map').getAttribute('aria-label')).toBe("Map of the United Kingdom, showing an area of " + radius / 1000 + "km around the latitude " + latitude.toFixed(2)+", longitude "+longitude.toFixed(2));
+        addingFeaturesToMap(L, mymap, bleed_circle, circle, pinpoint);
+        expect(bleed_circle.addTo).toHaveBeenCalledWith(mymap);
+        expect(circle.addTo).toHaveBeenCalledWith(mymap);
+        expect(pinpoint.addTo).toHaveBeenCalledWith(mymap);
+        expect(mymap.fitBounds).toHaveBeenCalledWith(bleed_circle.getBounds(), {padding: [1,1]});
+        expected_label = "Map of the United Kingdom, showing an area of " + radius / 1000 + "km around the latitude " + latitude.toFixed(2)+", longitude "+longitude.toFixed(2);
+        expect(document.getElementById('area-list-map').getAttribute('aria-label')).toBe(expected_label);
     });
 
 });
 
 
 describe('Easting and northing area is created on the map when easting and northing searched and radius entered', () => {
-
-    test("User has entered the postcode and radius and then searched to generate the leaflet circle and marker on the map", () => {
+    let bleed_circle = {addTo: jest.fn(), getBounds: jest.fn().mockReturnValue({extend: jest.fn()})};
+    let circle = {addTo: jest.fn(), getBounds: jest.fn().mockReturnValue({extend: jest.fn()})};
+    let pinpoint = {addTo: jest.fn(), getLatLng: jest.fn().mockReturnValue([0,0])};
+    test("User has entered the easting, northing and radius and then searched to generate the leaflet circle and marker on the map", () => {
 
         createEastingNorthingArea(L, proj4, easting, northing, radius, bleed, mapElement);
-        expect(document.getElementById('area-list-map').getAttribute('aria-label')).toBe("Map of the United Kingdom, showing an area of "+radius/1000+"km around the easting of "+easting+", northing of "+northing);
+        addingFeaturesToMap(L, mymap, bleed_circle, circle, pinpoint);
+        expect(bleed_circle.addTo).toHaveBeenCalledWith(mymap);
+        expect(circle.addTo).toHaveBeenCalledWith(mymap);
+        expect(pinpoint.addTo).toHaveBeenCalledWith(mymap);
+        expect(mymap.fitBounds).toHaveBeenCalledWith(bleed_circle.getBounds(), {padding: [1,1]});
+        expected_label = "Map of the United Kingdom, showing an area of "+radius/1000+"km around the easting of "+easting+", northing of "+northing;
+        expect(document.getElementById('area-list-map').getAttribute('aria-label')).toBe(expected_label);
     });
 
 });
 
 describe('Postcode area is created on the map when postcode searched and radius entered', () => {
     let centroid = [54, -2];
+    let bleed_circle = {addTo: jest.fn(), getBounds: jest.fn().mockReturnValue({extend: jest.fn()})};
+    let circle = {addTo: jest.fn(), getBounds: jest.fn().mockReturnValue({extend: jest.fn()})};
+    let pinpoint = {addTo: jest.fn(), getLatLng: jest.fn().mockReturnValue([0,0])};
     test("User has entered the postcode and radius and then searched to generate the leaflet circle and marker on the map", () => {
         createPostcodeArea(L, centroid, radius, bleed, postcode);
-        expect(document.getElementById('area-list-map').getAttribute('aria-label')).toBe("Map of the United Kingdom, showing an area of " + radius / 1000 + "km around the postcode " + postcode);
+        addingFeaturesToMap(L, mymap, bleed_circle, circle, pinpoint);
+        expect(bleed_circle.addTo).toHaveBeenCalledWith(mymap);
+        expect(circle.addTo).toHaveBeenCalledWith(mymap);
+        expect(pinpoint.addTo).toHaveBeenCalledWith(mymap);
+        expect(mymap.fitBounds).toHaveBeenCalledWith(bleed_circle.getBounds(), {padding: [1,1]});
+        expected_label = "Map of the United Kingdom, showing an area of " + radius / 1000 + "km around the postcode " + postcode;
+        expect(document.getElementById('area-list-map').getAttribute('aria-label')).toBe(expected_label);
     });
 
 });
 
 describe('Pinpoint marker is added to leaflet map for postcode centroid', () => {
-    let centroid = [54, -2];
-    test("User has entered the postcode and radius and then searched to generate the leaflet circle and marker on the map", () => {
+    centroid = [54, -2];
+    test("User has entered the postcode then searched to generate the marker on the map", () => {
         addingPostcodeCentroidMarkerToMap(L, mymap, centroid);
-        // Check HTML for marker etc
+        expect(mymap.setView).toHaveBeenCalledWith({"lat": centroid[0], "lng": centroid[1]}, 13);
+        expect(mymap.addLayer).toHaveBeenCalledWith(pinpoint);
     });
-
 });
 
-// describe('Features are added to leaflet map', () => {
-//     test("User has entered the postcode and radius and then searched to generate the leaflet circle and marker on the map", () => {
-//         let {circle, bleed_circle, pinpoint} = createLatitudelongitudeArea(L, latitude, longitude, radius, bleed, mapElement);
-//         addingFeaturesToMap(L, mymap, bleed_circle, circle, pinpoint);
-//         // Check HTML
-//     });
-// });
+describe('Area is added to area list', () => {
+    let label = "Test label";
+    test("User has added an area to the Leaflet map", () => {
+        appendAreaToAreaList(label);
+        expect(document.querySelector('.area-list-item').textContent).toBe(label);
+    });
+});
