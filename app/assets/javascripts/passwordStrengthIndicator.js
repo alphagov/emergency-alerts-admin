72fe4cabea72dbf558e861b5b9a53cf8a709bd4d
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2024-09-20 17:00:29 +0100

    Added password strength indicator component

---
+++ app/assets/javascripts/passwordStrengthIndicator.js
@@ -0,0 +1,61 @@
+(function (window) {
+    "use strict";
+
+    const setColour = function(entropy) {
+        if (entropy < 33 ){
+            strengthBar.style.setProperty('--progress-bar-color', "#D4351C");
+        } else if (33 < entropy < 66) {
+            strengthBar.style.setProperty('--progress-bar-color', "#F47738");
+        }
+        if (entropy > 70) {
+            strengthBar.style.setProperty('--progress-bar-color', '#00703C');
+        }
+    };
+
+    const setText = function(entropy) {
+        if (entropy < 33 ){
+            strengthText.innerText = "Weak";
+            strengthText.style.color = "#D4351C";
+        } else if (33 < entropy < 66) {
+            strengthText.innerText = "Medium";
+            strengthText.style.color = "#F47738";
+        }
+        if (entropy > 70) {
+            strengthText.innerText = "Strong";
+            strengthText.style.color = "#00703C";
+        }
+    };
+
+    const _includesChar = function(text, charlist) {
+        for (let i = 0;i < text.length; i++) {
+        if (charlist.includes(text[i]))
+            return true;
+        }
+        return false;
+    };
+
+    const calculateEntropy = function(password) {
+        if (typeof password !== "string")
+        return 0;
+        let pool = 0;
+        if (_includesChar(password, lcase))
+        pool += lcase.length;
+        if (_includesChar(password, ucase))
+        pool += ucase.length;
+        if (_includesChar(password, numb))
+        pool += numb.length;
+        if (_includesChar(password, symbol))
+        pool += symbol.length;
+        if (!_includesChar(password, lcase + ucase + numb + symbol))
+        pool += 100;
+        if (pool == 0)
+        return 0;
+        return Math.round(password.length * Math.log(pool) / Math.LN2);
+    };
+
+    window.GOVUK.setColour = setColour;
+    window.GOVUK.setText = setText;
+    window.GOVUK._includesChar = _includesChar;
+    window.GOVUK.calculateEntropy = calculateEntropy;
+
+})(window);
