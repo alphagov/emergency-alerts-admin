4becf43a79e73ce1efd311954cf7f6a5c6f45a79
Jonathan Owens <jonathan.owens@digital.cabinet-office.gov.uk>
2024-10-16 13:40:03 +0100

    remove job endpoints from module

--- app/main/views/jobs.py
+++ app/main/views/jobs.py
@@ -7,144 +7,23 @@ from emergency_alerts_utils.template import (
     LetterPreviewTemplate,
     SMSBodyPreviewTemplate,
 )
-from flask import (
-    Response,
-    abort,
-    flash,
-    jsonify,
-    redirect,
-    render_template,
-    request,
-    stream_with_context,
-    url_for,
-)
+from flask import Response, abort, jsonify, render_template, request, url_for
 from flask_login import current_user
 from markupsafe import Markup
-from notifications_python_client.errors import HTTPError
 
-from app import (
-    current_service,
-    format_datetime_short,
-    format_thousands,
-    notification_api_client,
-    service_api_client,
-)
+from app import current_service, notification_api_client, service_api_client
 from app.formatters import get_time_left, message_count_noun
 from app.main import main
 from app.main.forms import SearchNotificationsForm
-from app.models.job import Job
 from app.utils import parse_filter_args, set_status_filters
 from app.utils.csv import generate_notifications_csv
-from app.utils.letters import get_letter_printing_statement, printing_today_or_tomorrow
+from app.utils.letters import get_letter_printing_statement
 from app.utils.pagination import (
     generate_next_dict,
     generate_previous_dict,
     get_page_from_request,
 )
 from app.utils.user import user_has_permissions
-
-
-@main.route("/services/<uuid:service_id>/jobs")
-@user_has_permissions()
-def view_jobs(service_id):
-    return redirect(url_for(".index"))
-
-
-@main.route("/services/<uuid:service_id>/jobs/<uuid:job_id>")
-@user_has_permissions()
-def view_job(service_id, job_id):
-    job = Job.from_id(job_id, service_id=current_service.id)
-    if job.cancelled:
-        abort(404)
-
-    filter_args = parse_filter_args(request.args)
-    filter_args["status"] = set_status_filters(filter_args)
-
-    just_sent_message = "Your {} been sent. Printing starts {} at 5:30pm.".format(
-        "letter has" if job.notification_count == 1 else "letters have", printing_today_or_tomorrow(job.created_at)
-    )
-
-    return render_template(
-        "views/jobs/job.html",
-        job=job,
-        status=request.args.get("status", ""),
-        updates_url=url_for(
-            ".view_job_updates",
-            service_id=service_id,
-            job_id=job.id,
-            status=request.args.get("status", ""),
-        ),
-        partials=get_job_partials(job),
-        just_sent=request.args.get("just_sent") == "yes",
-        just_sent_message=just_sent_message,
-    )
-
-
-@main.route("/services/<uuid:service_id>/jobs/<uuid:job_id>.csv")
-@user_has_permissions("view_activity")
-def view_job_csv(service_id, job_id):
-    job = Job.from_id(job_id, service_id=service_id)
-    filter_args = parse_filter_args(request.args)
-    filter_args["status"] = set_status_filters(filter_args)
-
-    return Response(
-        stream_with_context(
-            generate_notifications_csv(
-                service_id=service_id,
-                job_id=job_id,
-                status=filter_args.get("status"),
-                page=request.args.get("page", 1),
-                page_size=5000,
-                format_for_csv=True,
-                template_type=job.template_type,
-            )
-        ),
-        mimetype="text/csv",
-        headers={
-            "Content-Disposition": 'inline; filename="{} - {}.csv"'.format(
-                job.template["name"], format_datetime_short(job.created_at)
-            )
-        },
-    )
-
-
-@main.route("/services/<uuid:service_id>/jobs/<uuid:job_id>", methods=["POST"])
-@user_has_permissions("send_messages")
-def cancel_job(service_id, job_id):
-    Job.from_id(job_id, service_id=service_id).cancel()
-    return redirect(url_for("main.service_dashboard", service_id=service_id))
-
-
-@main.route("/services/<uuid:service_id>/jobs/<uuid:job_id>/cancel", methods=["GET", "POST"])
-@user_has_permissions()
-def cancel_letter_job(service_id, job_id):
-    if request.method == "POST":
-        job = Job.from_id(job_id, service_id=service_id)
-
-        if job.status != "finished" or job.notifications_created < job.notification_count:
-            flash("We are still processing these letters, please try again in a minute.", "try again")
-            return view_job(service_id, job_id)
-        try:
-            number_of_letters = job.cancel()
-        except HTTPError as e:
-            flash(e.message, "dangerous")
-            return redirect(url_for("main.view_job", service_id=service_id, job_id=job_id))
-        flash(
-            "Cancelled {} letters from {}".format(format_thousands(number_of_letters), job.original_file_name),
-            "default_with_tick",
-        )
-        return redirect(url_for("main.service_dashboard", service_id=service_id))
-
-    flash("Are you sure you want to cancel sending these letters?", "cancel")
-    return view_job(service_id, job_id)
-
-
-@main.route("/services/<uuid:service_id>/jobs/<uuid:job_id>.json")
-@user_has_permissions()
-def view_job_updates(service_id, job_id):
-    job = Job.from_id(job_id, service_id=service_id)
-
-    return jsonify(**get_job_partials(job))
 
 
 @main.route("/services/<uuid:service_id>/notifications", methods=["GET", "POST"])
