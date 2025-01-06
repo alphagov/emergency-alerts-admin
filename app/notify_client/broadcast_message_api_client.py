66abe9301c75f28c40505fc062a93cfd71458f7f
leylayaltiligil <151185790+leylayaltiligil@users.noreply.github.com>
2025-01-06 10:50:35 +0000

    EAS-2419 Reason for rejection (#210)
    * Added reason for rejection form
    
    * Added rejection with reason function
    
    * Added required rejection properties to BroadcastMessage class
    
    * Passing in rejection reason, if provided, to api route
    
    * Added form to view-message page for rejecting alert
    
    * Displaying rejection reason for each alert in rejected alerts
    
    * Updated rejection reason form
    
    * Added reason for rejection to alerts authored by others also
    
    * Added class for dashboard-table item properties
    
    * Removed form title - temp as may be required
    
    * Added additional text to rejected alerts page
    
    * Added page title to pending alert page
    
    * Added additional fields to rejected alerts items
    
    * Added method to route
    
    * Removed retrieving name from User model to solve performance issue
    
    * No longer need to get name as name is now an attribute
    
    * No need to reference User name as now returned & margin top added
    
    * Changed condition in jinja
    
    * Added additional fields to broadcast message json for tests
    
    * Added rejection reason to update message status mock
    
    * Adjusted failing tests
    
    * Updated endpoints & tests
    
    * Removed required input - will confirm whether message optional or not
    
    * Added api client tests for rejection
    
    * Added id elements to form & adjusted condition for page title
    
    * Updated broadcast message json
    
    * Adjusted existing tests
    
    * Added rejection test
    
    * Added class to rejection reason div for truncation
    
    * Added data mandatory validator to form
    
    * Added conditional for details open attribute
    
    * Added route for discarding alert to allow rejection without reason
    
    * Adjusted rejection with reason function
    
    * Added a new API function for updating alert status with reason
    
    * Changed link url for discarding alert
    
    * Added and adjusted tests
    
    * Changed rejection reason to h2
    
    * Adjusted text
    
    * Ensuring form passed into view-message html
    
    * Adjusted stopped by text and styling on view-message page
    
    * Adjusted text for rejected by when no rejected_by_id
    
    * Adjusted rejected_by conditional
    
    * Adjusted created by text
    
    * Adjusted hint text for rejection reason form
    
    * Simplifed logic to avoid duplication
    
    * Adjusted text on rejected alerts page

--- app/notify_client/broadcast_message_api_client.py
+++ app/notify_client/broadcast_message_api_client.py
@@ -1,3 +1,5 @@
+from notifications_python_client.errors import HTTPError
+
 from app.notify_client import AdminAPIClient, _attach_current_user
 
 
@@ -31,10 +33,10 @@ class BroadcastMessageAPIClient(AdminAPIClient):
         return broadcast_message
 
     def get_broadcast_messages(self, service_id):
-        return self.get(f"/service/{service_id}/broadcast-message")["broadcast_messages"]
+        return self.get(f"/service/{service_id}/broadcast-message/messages")["broadcast_messages"]
 
     def get_broadcast_message(self, *, service_id, broadcast_message_id):
-        return self.get(f"/service/{service_id}/broadcast-message/{broadcast_message_id}")
+        return self.get(f"/service/{service_id}/broadcast-message/message={broadcast_message_id}")
 
     def update_broadcast_message(self, *, service_id, broadcast_message_id, data):
         self.post(
@@ -52,6 +54,24 @@ class BroadcastMessageAPIClient(AdminAPIClient):
             f"/service/{service_id}/broadcast-message/{broadcast_message_id}/status",
             data=data,
         )
+
+    def update_broadcast_message_status_with_reason(
+        self, status, *, service_id, broadcast_message_id, rejection_reason
+    ):
+        data = _attach_current_user(
+            {
+                "status": status,
+                **({"rejection_reason": rejection_reason} if rejection_reason else {}),
+            }
+        )
+        try:
+            self.post(
+                f"/service/{service_id}/broadcast-message/{broadcast_message_id}/status-with-reason",
+                data=data,
+            )
+        except HTTPError as e:
+            if e.status_code == 400:
+                raise e
 
 
 broadcast_message_api_client = BroadcastMessageAPIClient()
