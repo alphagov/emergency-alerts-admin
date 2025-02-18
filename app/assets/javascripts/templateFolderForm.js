97d35aa01bef73d7e282f17d88fd7c87b94af6b9
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2025-02-18 11:40:02 +0000

    This ensures parent folder ID is captured from pathanme, if it exists in pathname, and is passed into python route as param  for creation of new template

--- app/assets/javascripts/templateFolderForm.js
+++ app/assets/javascripts/templateFolderForm.js
@@ -189,12 +189,16 @@
 
     this.$singleNotificationChannel = (document.querySelector('div[id=add_new_template_form]')).getAttribute("data-channel");
     this.$singleChannelService = (document.querySelector('div[id=add_new_template_form]')).getAttribute("data-service");
+    const parts = window.location.pathname.split("/").filter(Boolean);
+    this.$parentFolderID = parts.includes('folders') ? parts.slice(parts.indexOf('folders') + 1).join('/') : false;
 
     this.actionButtonClicked = function(event) {
       event.preventDefault();
       this.currentState = $(event.currentTarget).val();
 
-      if (event.currentTarget.value === 'add-new-template' && this.$singleNotificationChannel) {
+      if (event.currentTarget.value === 'add-new-template' && this.$singleNotificationChannel && this.$parentFolderID) {
+        window.location = "/services/" + this.$singleChannelService + "/templates/folders/" + this.$parentFolderID + "/add-" + this.$singleNotificationChannel;
+      } else if (event.currentTarget.value === 'add-new-template' && this.$singleNotificationChannel) {
         window.location = "/services/" + this.$singleChannelService + "/templates/add-" + this.$singleNotificationChannel;
       } else {
         if (this.stateChanged()) {
