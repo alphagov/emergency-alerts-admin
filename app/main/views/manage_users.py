2e84f3812f116aa6aa1500814814878ed11981bc
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2025-04-07 17:37:26 +0100

    Adjusts invite_usder function to account for invited pending users

--- app/main/views/manage_users.py
+++ app/main/views/manage_users.py
@@ -59,7 +59,15 @@ def invite_user(service_id, user_id=None):
     if user_id:
         user_to_invite = User.from_id(user_id)
     elif form.email_address.data:
-        user_to_invite = User.from_email_address_invited(form.email_address.data)
+        # Check if user with email address already exists
+        if User.from_email_address_invited(form.email_address.data) is not None:
+            user_to_invite = User.from_email_address_invited(form.email_address.data)
+        elif current_service.invite_pending_for(form.email_address.data):
+            # Checks this email address hasn't already been invited to service
+            return render_template(
+                "views/user-already-invited.html",
+                user_to_invite=None,
+            )
     if user_to_invite is not None:
         if user_to_invite.belongs_to_service(current_service.id):
             return render_template(
