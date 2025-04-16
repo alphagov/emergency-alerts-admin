61a216322d0f949d547b475317f8cf6f013b2351
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2025-04-16 16:21:23 +0100

    Fix sneaky Gulp lints

--- app/assets/javascripts/permissionFormSubmitButton.js
+++ app/assets/javascripts/permissionFormSubmitButton.js
@@ -5,26 +5,26 @@
   // It expects to be given a parent <form> element, and will note those with "data-permission-sensitive".
   Modules.PermissionFormSubmitButton = function () {
     this.start = function (permissionForm) {
-      var $permissionForm = $(permissionForm)
+      var $permissionForm = $(permissionForm);
       var submitButton = $permissionForm.find(":submit");
       var initialSubmitButtonText = submitButton.text();
 
       var sensitivePermissionCheckboxes = $("[data-permission-sensitive]");
-      var sensitivePermissionValues = sensitivePermissionCheckboxes.map(function () { return this.value }).get();
+      var sensitivePermissionValues = sensitivePermissionCheckboxes.map(function () { return this.value; }).get();
 
       // Note the form's initial state for editing a user
-      var initialPermissions = $("#permissions_field input:checked").map(function () { return this.value }).get();
+      var initialPermissions = $("#permissions_field input:checked").map(function () { return this.value; }).get();
 
       sensitivePermissionCheckboxes.on("change", function () {
-        var currentPermissions = $("#permissions_field input:checked").map(function () { return this.value }).get();
-        var newPermissions = currentPermissions.filter(function (permission) { return !initialPermissions.includes(permission) })
+        var currentPermissions = $("#permissions_field input:checked").map(function () { return this.value; }).get();
+        var newPermissions = currentPermissions.filter(function (permission) { return !initialPermissions.includes(permission); });
 
-        if (newPermissions.some(function (permission) { return sensitivePermissionValues.includes(permission) })) {
+        if (newPermissions.some(function (permission) { return sensitivePermissionValues.includes(permission); })) {
           submitButton.text("Submit for approval");
         } else {
           submitButton.text(initialSubmitButtonText);
         }
-      })
+      });
     };
   };
 
