65a58205a74dcd5f63a5e494522258a5fbdd165c
leylayaltiligil <151185790+leylayaltiligil@users.noreply.github.com>
2025-04-23 09:21:57 +0100

    EAS-2575 Extend inactivity timeout (#241)
    * Changes inactivity timeout period from 28 mins to 58
    
    * Adds INACTIVITY_WARNING_MINS to config and comments to add context for each parameter
    
    * Added extra parameters to template to pass into js
    
    * Renames inactivity logout dialog file
    
    * Adds inactivity warning dialog
    
    * Adds parameter for functional tests - need to adjust timings
    
    * Adds variable to admin_template
    
    * Adds parameter to setup.js
    
    * Adds inactivityWarning related functions to module
    
    * Adds inactivityWarningDialog params to tests and adjusts existing tests
    
    * Adds format_timeout_datetime formatter
    
    * Adds session attribute for session_end_utc
    
    * Passing session_expiry_time into inactivityLogoutDialog
    
    * Removes redundant setTimeRemaining function & adds comments
    
    * Removes change added for testing
    
    * Removes unused formatTime function
    
    * Removes creation of session_end_utc session attribute
    
    * Removes redundant formatter
    
    * No longer passing in session_expiry_time
    
    * Adjusts dialog text and removes input parameter that's no longer used
    
    * Removes non-existent formatter
    
    * Adds close button instead of link
    
    * Adjustments to last active functions & new function
    
    * Renames and organises functions within file
    
    * Updates tests
    
    * Updates functions used in tests
    
    * Adds govukTag back after accidentally removing import
    
    * Adjusts tests to account for additional button on the page
    
    * Changes text to 30 mins
    
    * Refactoring & adds comments
    
    * Inactivity warning to appear after 30 mins now instead of 28
    
    * Renames timeout files, adds comments to sessionTimeout file to document functionality
    
    * Renames function

--- tests/javascripts/support/setup.js
+++ tests/javascripts/support/setup.js
@@ -11,7 +11,8 @@ require('../../../app/assets/javascripts/modules.js');
 const mockDate = new Date();
 window.logged_in_at = mockDate;
 
-inactivity_mins = 0.5;
+inactivity_mins = 0.8;
+inactivity_warning_mins = 0.5;
 inactivity_warning_duration = 0.5;
 expiry_warning_mins = 1.5;
 permanent_session_lifetime = 2;
