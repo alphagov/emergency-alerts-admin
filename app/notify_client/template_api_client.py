608224e782580e9f228d5088251a3c61a80e6eda
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2025-08-14 13:54:36 +0100

    Template fields default set to empty string, not None

--- app/notify_client/template_api_client.py
+++ app/notify_client/template_api_client.py
@@ -2,7 +2,7 @@ from app.notify_client import AdminAPIClient, _attach_current_user
 
 
 class TemplateAPIClient(AdminAPIClient):
-    def create_template(self, service_id, reference=None, content=None, parent_folder_id=None, areas=None):
+    def create_template(self, service_id, reference="", content="", parent_folder_id=None, areas=None):
         data = {
             "reference": reference,
             "template_type": "broadcast",
