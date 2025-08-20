96eba8a326c07ec41d87c1eae2f238e7045beeca
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2025-08-20 16:26:00 +0100

    Removes methods now added to template_api_client

--- app/notify_client/service_api_client.py
+++ app/notify_client/service_api_client.py
@@ -100,31 +100,6 @@ class ServiceAPIClient(AdminAPIClient):
         data = _attach_current_user({})
         return self.delete(endpoint, data)
 
-    def create_service_template(self, reference, type_, content, service_id, parent_folder_id=None):
-        """
-        Create a service template.
-        """
-        data = {
-            "reference": reference,
-            "template_type": type_,
-            "content": content,
-            "service": service_id,
-        }
-        if parent_folder_id:
-            data.update({"parent_folder_id": parent_folder_id})
-        data = _attach_current_user(data)
-        endpoint = "/service/{0}/template".format(service_id)
-        return self.post(endpoint, data)
-
-    def update_service_template(self, id_, reference, type_, content, service_id):
-        """
-        Update a service template.
-        """
-        data = {"id": id_, "reference": reference, "template_type": type_, "content": content, "service": service_id}
-        data = _attach_current_user(data)
-        endpoint = "/service/{0}/template/{1}".format(service_id, id_)
-        return self.post(endpoint, data)
-
     def redact_service_template(self, service_id, id_):
         return self.post(
             "/service/{}/template/{}".format(service_id, id_),
@@ -138,55 +113,11 @@ class ServiceAPIClient(AdminAPIClient):
         data = _attach_current_user(data)
         return self.post("/service/{0}/template/{1}".format(service_id, template_id), data)
 
-    def get_service_template(self, service_id, template_id, version=None):
-        """
-        Retrieve a service template.
-        """
-        endpoint = "/service/{service_id}/template/{template_id}".format(service_id=service_id, template_id=template_id)
-        if version:
-            endpoint = "{base}/version/{version}".format(base=endpoint, version=version)
-        return self.get(endpoint)
-
-    def get_service_template_versions(self, service_id, template_id):
-        """
-        Retrieve a list of versions for a template
-        """
-        endpoint = "/service/{service_id}/template/{template_id}/versions".format(
-            service_id=service_id, template_id=template_id
-        )
-        return self.get(endpoint)
-
     def get_precompiled_template(self, service_id):
         """
         Returns the precompiled template for a service, creating it if it doesn't already exist
         """
         return self.get("/service/{}/template/precompiled".format(service_id))
-
-    def get_service_templates(self, service_id):
-        """
-        Retrieve all templates for service.
-        """
-        endpoint = "/service/{service_id}/template?detailed=False".format(service_id=service_id)
-        return self.get(endpoint)
-
-    # This doesn’t need caching because it calls through to a method which is cached
-    def count_service_templates(self, service_id, template_type=None):
-        return len(
-            [
-                template
-                for template in self.get_service_templates(service_id)["data"]
-                if (not template_type or template["template_type"] == template_type)
-            ]
-        )
-
-    def delete_service_template(self, service_id, template_id):
-        """
-        Set a service template's archived flag to True
-        """
-        endpoint = "/service/{0}/template/{1}".format(service_id, template_id)
-        data = {"archived": True}
-        data = _attach_current_user(data)
-        return self.post(endpoint, data=data)
 
     # Temp access of service history data. Includes service and api key history
     def get_service_history(self, service_id):
