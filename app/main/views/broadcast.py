6528ea6bfed668ff2292ba31a6d730c841b65728
leylayaltiligil <leyla.yaltiligil@digital.cabinet-office.gov.uk>
2025-10-03 10:17:36 +0100

    Replaces repeated invalid area error text with constant

--- app/main/views/broadcast.py
+++ app/main/views/broadcast.py
@@ -35,6 +35,7 @@ from app.models.broadcast_message import BroadcastMessage, BroadcastMessages
 from app.models.template import Template
 from app.utils import service_has_permission
 from app.utils.broadcast import (
+    INVALID_AREA_ERROR_TEXT,
     _get_back_link_from_view_broadcast_endpoint,
     check_for_missing_fields,
     format_areas_list,
@@ -502,13 +503,7 @@ def preview_broadcast_message(service_id, broadcast_message_id):
     areas = format_areas_list(broadcast_message.areas)
 
     if is_custom_broadcast and not broadcast_message.areas.is_valid_area():
-        errors = [
-            {
-                "text": "The area used is invalid and the alert cannot be sent. If the alert was created"
-                " through the API, report it to the alert creator. Otherwise report it to the "
-                "Emergency Alerts team."
-            }
-        ]
+        errors = [{"text": INVALID_AREA_ERROR_TEXT}]
         return render_preview_alert_page(broadcast_message, is_custom_broadcast, areas, errors)
 
     if request.method == "POST":
@@ -549,13 +544,7 @@ def submit_broadcast_message(service_id, broadcast_message_id):
         return render_current_alert_page(broadcast_message, hide_stop_link=True, errors=errors)
 
     if type(broadcast_message.areas) is CustomBroadcastAreas and not broadcast_message.areas.is_valid_area():
-        errors = [
-            {
-                "text": "The area used is invalid and the alert cannot be sent. If the alert was created"
-                " through the API, report it to the alert creator. Otherwise report it to the "
-                "Emergency Alerts team."
-            }
-        ]
+        errors = [{"text": INVALID_AREA_ERROR_TEXT}]
         return render_current_alert_page(broadcast_message, hide_stop_link=True, errors=errors)
 
     broadcast_message.request_approval()
