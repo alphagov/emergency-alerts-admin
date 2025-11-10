6e11b97dacf1abc22e2aa07f1647eb7fdee1315c
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2025-11-10 10:34:29 +0000

    Refactor OTEL_PYTHON_DISTRO and co

--- scripts/start.sh
+++ scripts/start.sh
@@ -8,7 +8,7 @@ function configure_container_role(){
 
 function run_admin(){
     cd $DIR_ADMIN;
-    . $VENV_ADMIN/bin/activate && OTEL_PYTHON_DISTRO="aws_distro" OTEL_PYTHON_CONFIGURATOR="aws_configurator" opentelemetry-instrument flask run -p 6012 --host=0.0.0.0
+    . $VENV_ADMIN/bin/activate && opentelemetry-instrument flask run -p 6012 --host=0.0.0.0
 }
 
 if [[ ! -z $DEBUG ]]; then
