436bf43393cbdaa87192dff9681e949a260c877e
James Lakin <james.lakin@digital.cabinet-office.gov.uk>
2026-07-27 15:38:21 +0100

    EAS-3100: Admin: Add debugpy and DEBUGPY_PORT env (#429)
    This allows attaching debuggers to running containers in localenv.

--- scripts/start.sh
+++ scripts/start.sh
@@ -6,9 +6,15 @@ function configure_container_role(){
     aws configure set default.region ${AWS_REGION:-eu-west-2}
 }
 
+PYTHON_COMMAND="python -m"
+if [[ ! -z $DEBUGPY_PORT ]]; then
+    echo "Starting with debugpy on port $DEBUGPY_PORT"
+    PYTHON_COMMAND="python -Xfrozen_modules=off -m debugpy --listen 0.0.0.0:${DEBUGPY_PORT} -m"
+fi
+
 function run_admin(){
     cd $DIR_ADMIN;
-    . $VENV_ADMIN/bin/activate && opentelemetry-instrument flask run -p 6012 --host=0.0.0.0
+    . $VENV_ADMIN/bin/activate && exec opentelemetry-instrument $PYTHON_COMMAND flask run -p 6012 --host=0.0.0.0
 }
 
 if [[ ! -z $DEBUG ]]; then
